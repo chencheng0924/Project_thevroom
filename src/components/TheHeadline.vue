30cfdb991e3eb213baa7d1e05ecee668b8f0403d
chencheng0924 <abs255667@gmail.com>
2021-07-13 20:39:20 +0800

    修正footer標題

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,45 +1,46 @@
 <template>
-    <div class="titlebox">
-            <div class="auctiontitle">{{ titleName }}
-                <div class="rect"></div>
-                <div class="anrect"></div>
-            </div>
+  <div class="titlebox">
+    <div class="auctiontitle">
+      {{ titleName }}
+      <div class="rect"></div>
+      <div class="anrect"></div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['titleName']
+  props: ['titleName']
 }
 </script>
 
 <style lang="scss">
-.titlebox{
+.titlebox {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 18%;
+  color: #ffffff;
+  .auctiontitle {
+    font-size: 40px;
+    font-weight: bold;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 18%;
-    color: #FFFFFF;
-        .auctiontitle{
-            font-size: 40px;
-            font-weight: bold;
-            display: flex;
-            .rect{
-            background-color: #F34841;
-            width: 13px;
-            height: 35px;
-            transform: skew(150deg);
-            margin-top: 12px;
-            margin-left: 20px;
-            }
-            .anrect{
-            background-color: #F34841;
-            width: 13px;
-            height: 35px;
-            transform: skew(150deg);
-            margin-top: 12px;
-            margin-left: 5px;
-            }
-        }
+    .rect {
+      background-color: #f34841;
+      width: 13px;
+      height: 35px;
+      transform: skew(150deg);
+      margin-top: 12px;
+      margin-left: 20px;
+    }
+    .anrect {
+      background-color: #f34841;
+      width: 13px;
+      height: 35px;
+      transform: skew(150deg);
+      margin-top: 12px;
+      margin-left: 5px;
+    }
+  }
 }
 </style>
