d75b248222114439907702642de049fcbfa38819
shen0421 <fred870421@gmail.com>
2021-07-24 04:19:11 +0800

    chore: add member article page

---
+++ src/components/MemberArticle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="article">
+    <div style="border: 1px solid #f20; width: 100%; margin-top: 50px">
+      <div style="border: 1px solid #000; width: 600px; height:900px; margin: 0 auto; border-radius: 10px"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.article {
+  box-sizing: border-box;
+  border: 2px solid #00f;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 1200px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ import SignUp from '../pages/SignUp.vue'
 import SignIn from '../pages/SignIn.vue'
 import MemberData from '../components/MemberData.vue'
 import MemberAuction from '../components/MemberAuction.vue'
+import MemberArticle from '../components/MemberArticle.vue'
 
 Vue.use(VueRouter)
 
@@ -132,6 +133,11 @@ const routes = [
     path: '/memberauction',
     name: 'MemberAuction',
     component: MemberAuction
+  },
+  {
+    path: '/memberarticle',
+    name: 'MemberArticle',
+    component: MemberArticle
   }
 ]
 
